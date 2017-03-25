188924f9ac6eab3fb58736d95450f9c65c7930d3
AlanUlises <amontes1@ucol.mx>
2017-03-24 19:52:26 -0600

    ProyectoPruebas

---
+++ ProyectoPruebas/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProyectoPruebas
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private int[] vesp = new int[] { 1, 2, 3, 4, 5 };
+        private Burbuja burbu = new Burbuja();
+
+        [TestMethod]
+        public void ordernado()
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            burbu.ordenar(arr);
+            CollectionAssert.AreEqual(vesp, arr);
+        }
+
+        [TestMethod]
+        public void intercambiado()
+        {
+            int[] arr = new int[] { 1, 3, 2, 4, 5 };
+            burbu.ordenar(arr);
+            CollectionAssert.AreEqual(vesp, arr);
+        }
+    }
+}
